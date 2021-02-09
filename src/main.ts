6bf725d6ce4517012bd2f9199a1b832721becd56
Adler Holanda <63509784+adlerholanda@users.noreply.github.com>
2021-02-08 22:57:08 -0300

    Source updated
    NA

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import Phaser from 'phaser'
+
+import GameScene from './scenes/GameScene'
+
+const config: Phaser.Types.Core.GameConfig = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 300 },
+			debug: false
+		}
+	},
+	scene: [GameScene]
+}
+
+export default new Phaser.Game(config)
